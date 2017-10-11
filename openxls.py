e4194a1b671ffc2be28c64df6e0db7ba34ddd7ca
yehanghanGE <yehang.han@ge.com>
2017-10-11 12:10:19 +0800

    getting rows and column from sheets

--- openxls.py
+++ openxls.py
@@ -1,13 +1,9 @@
 import openpyxl
-from openpyxl.utils import get_column_letter, column_index_from_string
-print(get_column_letter(1))
-print(get_column_letter(2))
-print(get_column_letter(27))
-print(get_column_letter(900))
-
 wb = openpyxl.load_workbook('example.xlsx')
 sheet = wb.get_sheet_by_name('Sheet1')
-print(get_column_letter(sheet.max_column))
+print(tuple(sheet['A1':'C3']))
 
-print(column_index_from_string('A'))
-print(column_index_from_string('AA'))
+for rowOfCellObjects in sheet['A1':'C3']:
+    for cellObj in rowOfCellObjects:
+        print(cellObj.coordinate,cellObj.value)
+    print('---END OF ROW---')
