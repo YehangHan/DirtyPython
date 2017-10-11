ba1119262f4cb20d1d9953ca9cb790c716baedb9
yehanghanGE <yehang.han@ge.com>
2017-10-11 12:51:41 +0800

    fixed old version issues

--- openxls.py
+++ openxls.py
@@ -1,9 +1,14 @@
 import openpyxl
 wb = openpyxl.load_workbook('example.xlsx')
-sheet = wb.get_sheet_by_name('Sheet1')
-print(tuple(sheet['A1':'C3']))
+# sheet = wb.get_sheet_by_name('Sheet1')
+# print(tuple(sheet['A1':'C3']))
 
-for rowOfCellObjects in sheet['A1':'C3']:
-    for cellObj in rowOfCellObjects:
-        print(cellObj.coordinate,cellObj.value)
-    print('---END OF ROW---')
+# for rowOfCellObjects in sheet['A1':'C3']:
+#     for cellObj in rowOfCellObjects:
+#         print(cellObj.coordinate,cellObj.value)
+#     print('---END OF ROW---')
+
+sheet = wb.active
+list(sheet.columns)[1]
+for cellObj in list(sheet.columns)[1]:
+    print(cellObj.value)
