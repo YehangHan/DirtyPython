01971ead6e29ef0daa46b34a30a1e4a85ac6ff3f
yehanghanGE <yehang.han@ge.com>
2017-10-23 21:12:55 +0800

    add new field

--- generateFile.py
+++ generateFile.py
@@ -1,7 +1,7 @@
 import os
 
 f = open("example.txt","w+")
-str = "field1,field2,field3,filed4\r"
+str = "field1,field2,field3,filed4,field5\r"
 while os.stat("example.txt").st_size <= 1e+9:
     f.write(str)
 
