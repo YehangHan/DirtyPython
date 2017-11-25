1d52753d948a3e1c618bcbdea166ea386925f8bd
coderhh <yehanghan@gmail.com>
2017-11-25 23:27:20 +0800

    udpate wordProcessor

--- wordProcessor.py
+++ wordProcessor.py
@@ -1,14 +1,38 @@
-from google.cloud import translate  
+from googletrans import Translator  
 from docx import Document
+import traceback
 
-translate_client = translate.Client()  
+
+def insert_paragraph_after(paragraphs, idx, text=None):
+    next_paragraph_idx = idx + 1
+    if idx == len(paragraphs):
+        return document.add_paragraph(text)
+    try:
+        next_paragraph = paragraphs[next_paragraph_idx]
+    except:
+        errorFile = open('errorInfo.txt', 'w')
+        errorFile.write(traceback.format_exc())
+        errorFile.close()
+        print(next_paragraph_idx)
+    return next_paragraph.insert_paragraph_before(text)
+
+translator = Translator() 
 
 target = 'zh-CN'  
+num = 0
+document = Document('translate3.docx')
+document.add_paragraph('')
+paragraphs = list(document.paragraphs)
+print(len(paragraphs))
 
-document = Document('example.docx')
-for para in document.paragraphs:
-    text = para.text
-    translation = translate_client.translate(text,target_language=target)
-    print(u'Text:{}'.format(text))  
-    print(u'translation:{}'.format(translation['translatedText']))    
+for idx, paragraph in enumerate(paragraphs):
+    text = paragraph.text
+    if text and (not text.startswith("图")):
+        translation = translator.translate(text,dest=target)
+        insert_paragraph_after(paragraphs, idx, translation.text)
+
+document.save('translated3.docx')     
+    
+    
+
    
